f9b66b14f78f6acc816c6d5f0cf6bf4250c92ddb
epwhwesq <62261900+epwhwesq@users.noreply.github.com>
2020-03-16 16:43:59 -0400

    added HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+
+if(userName.lower() == "eric" and userPassword == "BigFarts!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
+
+
